87ebc9cf0e03844578c5fe50fd280e41dfbd8aa6
Andryyyha <adrianvalir@gmail.com>
2017-10-20 19:08:16 +0400

    initial

---
+++ src/main/java/com/andryyyha/libsystem/LibSystemApplication.java
@@ -0,0 +1,12 @@
+package com.andryyyha.libsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/andryyyha/libsystem/LibSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.andryyyha.libsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
